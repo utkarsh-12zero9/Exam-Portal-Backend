b2fb3a650555091d255184715ca51b53309e89fd
utkarshkumarsingh120903@gmail.com <utkarshkumarsingh120903@gmail.com>
2026-09-01 21:30:00 +0530

    clean up

--- src/config/db.js
+++ src/config/db.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGODB_URI);
-    console.log(`📡 MongoDB Connected Successfully: ${conn.connection.host}`);
+    console.log(`📡 MongoDB Connected Successfully!`);
   } catch (error) {
     console.error(`❌ MongoDB Connection Error: ${error.message}`);
     process.exit(1); // Exit process with failure
